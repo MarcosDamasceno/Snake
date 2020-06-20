27ae77a1d3f504900ddf1a3b3173b4393582c648
meumd <meumds@hotmail.com>
2020-06-20 19:45:27 -0300

    Implementado colisões

--- snake.py
+++ snake.py
@@ -2,6 +2,11 @@ import pygame
 import random
 from pygame.locals import *
 
+
+def collision(c1, c2):
+    return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
+
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -15,7 +20,7 @@ snake = [(200, 200), (210, 200), (220, 200)]
 snake_skin = pygame.Surface((10, 10))
 snake_skin.fill((255, 255, 255))
 
-apple_pos = (random.randint(0, 590), random.randint(0, 590))
+apple_pos = (random.randint(0, 59)*10, random.randint(0, 59)*10)
 apple = pygame.Surface((10, 10))
 apple.fill((255, 0, 0))
 my_direction = LEFT
@@ -38,6 +43,14 @@ while True:
                 my_direction = RIGHT
             if event.key == K_q:
                 pygame.quit()
+
+    if collision(snake[0], apple_pos):
+        apple_pos = (random.randint(0, 59)*10, random.randint(0, 59)*10)
+        snake.append((0, 0))
+
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
     if my_direction == DOWN:
@@ -46,9 +59,6 @@ while True:
         snake[0] = (snake[0][0] + 10, snake[0][1])
     if my_direction == LEFT:
         snake[0] = (snake[0][0] - 10, snake[0][1])
-
-    for i in range(len(snake) - 1, 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
 
     screen.fill((0, 0, 0))
     screen.blit(apple, apple_pos)
